cde63d0bcc9fe1093697fdff4a8d9ed17fdf9315
PandaTrumpet <dsavontrumpet@gmail.com>
2025-02-04 22:44:21 +0100

    create home page

--- src/Page/Home/Home.tsx
+++ src/Page/Home/Home.tsx
@@ -1,5 +1,31 @@
+import css from "./Home.module.css";
+import background from "../../image/home_background.jpg";
+
+import { GoArrowUpRight } from "react-icons/go";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={css.homeCont}>
+      <div className={css.firstCont}>
+        <div className={css.titleCont}>
+          <h1 className={css.homeTitle}>
+            Make Life Easier <br />
+            for the Family:
+          </h1>
+          <p className={css.homeText}>
+            Find Babysitters Online for All Occasions
+          </p>
+          <button className={css.homeBtn}>
+            Get Started
+            <GoArrowUpRight color="#fbfbfb" size={24} />
+          </button>
+        </div>
+      </div>
+      <div
+        className={css.secondCont}
+        style={{ backgroundImage: `url(${background})` }}
+      ></div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import css from "./Navigation.module.css";
 import { NavLink } from "react-router-dom";
 import clsx from "clsx";
+import { useEffect, useRef } from "react";
 interface isActiveProps {
   isActive: boolean;
 }
@@ -9,32 +10,44 @@ const Navigation = () => {
   const buildLinkClass = ({ isActive }: isActiveProps) => {
     return clsx(css.link, isActive && css.active);
   };
-
+  const navRef = useRef<HTMLElement>(null);
+  const location = useLocation();
+  useEffect(() => {
+    if (navRef.current) {
+      if (location.pathname === "/") {
+        navRef.current.style.background = "transparent";
+      } else {
+        navRef.current.style.background = "#0957c3";
+      }
+    }
+  }, [location.pathname]);
   return (
-    <nav className={css.navCont}>
+    <nav ref={navRef} className={css.navCont}>
       <div className={css.logoContainer}>
         <Link to="/">Nanny.Services</Link>
       </div>
-      <ul className={css.navMenuList}>
-        <NavLink to="/" className={buildLinkClass}>
-          Home
-        </NavLink>
-        <NavLink to="/nannies" className={buildLinkClass}>
-          Nannies
-        </NavLink>
-        <NavLink to="/favorites" className={buildLinkClass}>
-          Favorites
-        </NavLink>
-      </ul>
+      <div className={css.loginAndLinkCont}>
+        <ul className={css.navMenuList}>
+          <NavLink to="/" className={buildLinkClass}>
+            Home
+          </NavLink>
+          <NavLink to="/nannies" className={buildLinkClass}>
+            Nannies
+          </NavLink>
+          <NavLink to="/favorites" className={buildLinkClass}>
+            Favorites
+          </NavLink>
+        </ul>
 
-      <ul className={css.registerCont}>
-        <li>
-          <button className={css.loginBtn}>Log In</button>
-        </li>
-        <li>
-          <button className={css.registerBtn}>Registration</button>
-        </li>
-      </ul>
+        <ul className={css.registerCont}>
+          <li>
+            <button className={css.loginBtn}>Log In</button>
+          </li>
+          <li>
+            <button className={css.registerBtn}>Registration</button>
+          </li>
+        </ul>
+      </div>
 
       <div className={css.userLoggedCont}>
         <div>
